0fbbd147a6c2134190455f9b11fc0f2225828522
SharpBlade <yepezedwin760@gmail.com>
2025-03-26 12:33:19 -0500

    Conexión con la base principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.MAIN_DB_HOST,
+      port: Number(process.env.MAIN_DB_PORT),
+      username: process.env.MAIN_DB_USER,
+      password: process.env.MAIN_DB_PASSWORD,
+      database: process.env.MAIN_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
