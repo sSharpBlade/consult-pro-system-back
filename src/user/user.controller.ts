a8af0bcdc2c82f6d6d7618565830ed926693fbfb
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2025-04-18 20:20:27 -0500

    decoradores guards para user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,32 +8,47 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  UseGuards,
+  SetMetadata,
+  Req,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entity/user.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOne(+id);
   }
 
   @Put(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -43,12 +58,16 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async remove(@Param('id') id: string): Promise<void> {
     await this.usersService.remove(+id);
   }
 
   @Post(':id/restore')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async restore(@Param('id') id: string): Promise<void> {
     await this.usersService.restore(+id);
   }
