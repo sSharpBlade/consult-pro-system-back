4da6f68b656860dcf73efb510ad8ddcb19a883c5
SharpBlade <yepezedwin760@gmail.com>
2025-04-19 19:43:11 -0500

    decoradores guards para receta medica

--- src/paymentReceipt/paymentreceipt.controller.ts
+++ src/paymentReceipt/paymentreceipt.controller.ts
@@ -9,19 +9,26 @@ import {
   HttpCode,
   HttpStatus,
   Query,
+  SetMetadata,
+  UseGuards,
 } from '@nestjs/common';
 import { PaymentReceiptsService } from './paymentreceipt.service';
 import { CreatePaymentReceiptDto } from './dto/create-paymentReceipt.dto';
 import { UpdatePaymentReceiptDto } from './dto/update-paymentReceipt.dto';
 import { PaymentReceipt } from './entity/paymentReceipt.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('payment-receipts')
+@UseGuards(JwtAuthGuard)
 export class PaymentReceiptsController {
   constructor(
     private readonly paymentReceiptsService: PaymentReceiptsService,
   ) {}
 
   @Post()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['doctor', 'patient'])
   async create(
     @Body() createDto: CreatePaymentReceiptDto,
   ): Promise<PaymentReceipt> {
@@ -29,6 +36,8 @@ export class PaymentReceiptsController {
   }
 
   @Get()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['doctor', 'patient'])
   async findAll(
     @Query('appointmentId') appointmentId?: string,
   ): Promise<PaymentReceipt[]> {
@@ -39,11 +48,15 @@ export class PaymentReceiptsController {
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['doctor', 'patient'])
   async findOne(@Param('id') id: string): Promise<PaymentReceipt> {
     return this.paymentReceiptsService.findOne(+id);
   }
 
   @Put(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['doctor'])
   async update(
     @Param('id') id: string,
     @Body() updateDto: UpdatePaymentReceiptDto,
@@ -52,12 +65,16 @@ export class PaymentReceiptsController {
   }
 
   @Delete(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['doctor'])
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: string): Promise<void> {
     await this.paymentReceiptsService.remove(+id);
   }
 
   @Post(':id/restore')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   @HttpCode(HttpStatus.NO_CONTENT)
   async restore(@Param('id') id: string): Promise<void> {
     await this.paymentReceiptsService.restore(+id);
