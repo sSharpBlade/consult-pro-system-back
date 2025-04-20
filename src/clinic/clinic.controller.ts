020e8f0d1cce6f4a348a26160da77d52cace1d66
SharpBlade <yepezedwin760@gmail.com>
2025-04-19 19:42:35 -0500

    decoradores guards para clinicas

--- src/clinic/clinic.controller.ts
+++ src/clinic/clinic.controller.ts
@@ -8,32 +8,45 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  SetMetadata,
+  UseGuards,
 } from '@nestjs/common';
 import { ClinicsService } from './clinic.service';
 import { CreateClinicDto } from './dto/create-clinic.dto';
 import { UpdateClinicDto } from './dto/update-clinic.dto';
 import { Clinic } from './entity/clinic.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('clinics')
+@UseGuards(JwtAuthGuard)
 export class ClinicsController {
   constructor(private readonly clinicsService: ClinicsService) {}
 
   @Post()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async create(@Body() createClinicDto: CreateClinicDto): Promise<Clinic> {
     return this.clinicsService.create(createClinicDto);
   }
 
   @Get()
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin', 'patient'])
   async findAll(): Promise<Clinic[]> {
     return this.clinicsService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin', 'patient', 'secretary', 'admin'])
   async findOne(@Param('id') id: string): Promise<Clinic> {
     return this.clinicsService.findOne(+id);
   }
 
   @Put(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   async update(
     @Param('id') id: string,
     @Body() updateClinicDto: UpdateClinicDto,
@@ -42,12 +55,16 @@ export class ClinicsController {
   }
 
   @Delete(':id')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: string): Promise<void> {
     await this.clinicsService.remove(+id);
   }
 
   @Post(':id/restore')
+  @UseGuards(RolesGuard)
+  @SetMetadata('roles', ['admin'])
   @HttpCode(HttpStatus.NO_CONTENT)
   async restore(@Param('id') id: string): Promise<void> {
     await this.clinicsService.restore(+id);
